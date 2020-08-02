71c74e91927a5f1347bc60d24799da8c1cb375e9
BackLitKeyboard <rasik.hasan@yahoo.com>
2020-08-02 20:52:24 +0600

    added thunk stuff

--- src/redux/userEvents.ts
+++ src/redux/userEvents.ts
@@ -1,4 +1,6 @@
-import { AnyAction } from "redux";
+import { AnyAction, Action } from "redux";
+import { ThunkAction } from "redux-thunk";
+import { RootState } from "./store";
 
 interface UserEvent {
   id: number;
@@ -11,6 +13,39 @@ interface UserEventState {
   byIds: Record<UserEvent["id"], UserEvent>; //userevent id
   allIds: UserEvent["id"][]; // array of list of ids of user events
 }
+
+const LOAD_REQUEST = "userEvents/load_request";
+
+interface LoadRequestAction extends Action<typeof LOAD_REQUEST> {}
+
+const LOAD_SUCCESS = "userEvents/load_success";
+
+interface LoadSuccessAction extends Action<typeof LOAD_SUCCESS> {
+  payload: {
+    events: UserEvent[];
+  };
+}
+
+export const loadUserEvents = (): ThunkAction<
+  void,
+  RootState,
+  undefined,
+  LoadRequestAction | LoadSuccessAction
+> => async (dispatch, getState) => {
+  dispatch({
+    type: LOAD_REQUEST,
+  });
+
+  try {
+    const response = await fetch("http://localhost:3001/events");
+    const events: UserEvent[] = await response.json(); // note: TS-API
+
+    dispatch({
+      type: LOAD_SUCCESS,
+      payload: { events },
+    });
+  } catch (error) {}
+};
 
 const initialState: UserEventState = {
   byIds: {},
